9a96383a7c663e41cb789638fc984e244d7bde2b
Dinh Nguyen Thien Vo <dinh.vo@marcossoftware.com>
2025-05-21 09:32:14 +0700

    added pipelineId and processorId to ProcessorExecutionRequest

--- Qilin.Core.PipelineProcessor.Abstractions/Execution/ProcessorExecutionRequest.cs
+++ Qilin.Core.PipelineProcessor.Abstractions/Execution/ProcessorExecutionRequest.cs
@@ -8,6 +8,8 @@ public class ProcessorExecutionRequest
     public PipelineProcessorConfig PipelineProcessorConfig { get; init; } = null!;
     public PipelineProcessorType PipelineProcessorType { get; init; }
     public string SubscriptionId { get; init; } = null!;
+    public string PipelineId { get; init; } = null!;
+    public string ProcessorId { get; init; } = null!;
     public Dictionary<string, Dictionary<string, string>>? FlowObjectAttributes { get; init; }
     [Obsolete("Use FlowObjectContents instead", false)]
     public object? FlowObjectContent { get; set; }
